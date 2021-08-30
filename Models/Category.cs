a4a22821b3f78f018c86bdb17331ce31ee432a1d
Al Armato <55386215+AleArmato@users.noreply.github.com>
2021-08-30 16:18:25 +0200

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -15,5 +15,6 @@ namespace Supermarket.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public HashSet<Product> Products;
     }
 }
